141f5f23308ccb7c4a5afdd656dfae9aa145c8ec
vengat <vengatmuthu1996@gmail.com>
2020-07-10 13:54:37 +0530

    first commit

---
+++ src/com/maven/ssl/HelloController.java
@@ -0,0 +1,13 @@
+package com.maven.ssl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+ 
+    @GetMapping("/hello")
+    public String getGreeting() {
+        return "Hello Spring World!";
+    }
+}
